877d2d6b3ebd20a7dbce1181c0d0c96c87548b37
KBSN21 <kubi123656@gmail.com>
2022-12-11 20:18:09 +0100

    second commit

--- main.js
+++ main.js
@@ -1,21 +1,8 @@
-window.onload = function () {
-	const scroll = new LocomotiveScroll({
-		el: document.querySelector('[data-scroll-container]'),
-		multiplier: 2,
-		offset: ['2%', '0'],
-		lerp: 0.015,
-		smooth: true,
-		direction: 'horizontal',
-		smartphone: {
-			direction: 'vertical',
-			class: 'sss',
-		},
-	});
-};
+window.addEventListener('DOMContentLoaded', function (event) {});
+let scroll = 0;
 
 function changeSource(url) {
 	const video = document.querySelector('video');
-	console.dir(video);
 	video.src = url;
 	video.play();
 }
@@ -27,3 +14,40 @@ if (media.matches) {
 } else {
 	changeSource('/assets/hero.mp4');
 }
+
+const menu = document.querySelector('#menu');
+const hamburger = document.querySelector('#hamburger');
+const menuItems = document.querySelectorAll('li');
+
+const toggleMenu = () => {
+	menu.classList.toggle('-top-[100vh]');
+	menu.classList.toggle('-top-0');
+};
+const scrollToSection = function (e) {
+	setTimeout(() => {
+		scroll.scrollTo(`#${this.dataset.scrollTo}`);
+	}, 300);
+};
+
+hamburger.addEventListener('click', toggleMenu);
+
+window.addEventListener('load', () => {
+	document.querySelector('.loader').outerHTML = '';
+	scroll = new LocomotiveScroll({
+		el: document.body,
+		multiplier: 2,
+		offset: ['2%', '0'],
+		lerp: 0.015,
+		smooth: true,
+		direction: 'horizontal',
+		smartphone: {
+			direction: 'vertical',
+			class: 'sss',
+		},
+	});
+
+	menuItems.forEach(element => {
+		element.addEventListener('click', toggleMenu);
+		element.addEventListener('click', scrollToSection);
+	});
+});
